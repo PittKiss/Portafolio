2caf8102c3e5f94afd52e27759cc92b31165e4b4
Peter Kiss <peterbotondkiss@gmail.com>
2022-03-13 16:27:17 +0100

    Añadido inyeccion de dependecnias

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portafolio.Models;
+using Portafolio.Servicios;
 using System.Diagnostics;
 
 namespace Portafolio.Controllers
@@ -7,30 +8,22 @@ namespace Portafolio.Controllers
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
+        private readonly RepositorioProyectos repositorio;
 
-		public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,RepositorioProyectos repositorio)
 		{
 			_logger = logger;
-		}
+            this.repositorio = repositorio;
+        }
 
 		public IActionResult Index()
 		{
-			var proyectos = ObtenerProyectos().Take(6).ToList();
+			
+			var proyectos = repositorio.ObtenerProyectos().Take(6).ToList();
 			var modelo = new HomeInicioViewModel { Proyectos = proyectos };
-			ViewBag.Tipo = "Minion ";
 			return View("Inicio", modelo);
 		}
-		private List<Proyecto> ObtenerProyectos() {
-
-			return new List<Proyecto> {
-				new Proyecto { Titulo = "Kungfu Pandih", Descripcion = "Hago Kungfu", Link="https://www.twitch.tv", ImagenURL="/img/kungfu.jpg"},
-				new Proyecto { Titulo = "Putazo", Descripcion = "Te muerdo y te pego", Link="https://www.twitch.tv", ImagenURL="/img/putazo.jpg"},
-				new Proyecto { Titulo = "Zing", Descripcion = "Te hago un zing horendo", Link="https://www.twitch.tv", ImagenURL="/img/zing.jpg"},
-				new Proyecto { Titulo = "Pandih en bikini", Descripcion = "Te digo 'Pandih en bikini tomando un martini'", Link="https://www.twitch.tv", ImagenURL="/img/bikini.jpg"},
-				new Proyecto { Titulo = "Pandih minipekka", Descripcion = "Te digo 'aña'", Link="https://www.twitch.tv", ImagenURL="/img/aña.jpg"},
-				new Proyecto { Titulo = "Pandih se va", Descripcion = "Me pongo la capucha y me voy por lo alto", Link="https://www.twitch.tv", ImagenURL="/img/capucha.gif"}
-			};
-		}
+		
 
 		public IActionResult Privacy()
 		{
---
+++ Servicios/RepositorioProyectos.cs
@@ -0,0 +1,20 @@
+﻿using Portafolio.Models;
+
+namespace Portafolio.Servicios
+{
+    public class RepositorioProyectos
+    {
+		public List<Proyecto> ObtenerProyectos()
+		{
+			return new List<Proyecto> {
+				new Proyecto { Titulo = "Kungfu Pandih", Descripcion = "Paso de ser una camarera del restaurante de pasta de mi padre a convertirme en un héroe de artes marciales.", Link="https://www.twitch.tv", ImagenURL="/img/kungfu.jpg"},
+				new Proyecto { Titulo = "Putazo", Descripcion = "Te invito a mi casa y en mi sótano te muerdo y te pego.", Link="https://www.twitch.tv", ImagenURL="/img/putazo.jpg"},
+				new Proyecto { Titulo = "Zing", Descripcion = "Te hago un zing con tu nombre y un dibujo horrible que te traumará.", Link="https://www.twitch.tv", ImagenURL="/img/zing.jpg"},
+				new Proyecto { Titulo = "Pandih en Clash Royale", Descripcion = "Me convierto en una carta de calidad especial en el Clash Royale.", Link="https://www.twitch.tv", ImagenURL="/img/carta.gif"},
+				new Proyecto { Titulo = "Pandih japonea", Descripcion = "Me convierto en japonesa y te digo 'aña' y 'onichan'.", Link="https://www.twitch.tv", ImagenURL="/img/aña.jpg"},
+				new Proyecto { Titulo = "Pandih se va", Descripcion = "Me pongo la capucha y me voy por lo alto.", Link="https://www.twitch.tv", ImagenURL="/img/capucha.gif"}
+			};
+		}
+
+	}
+}
